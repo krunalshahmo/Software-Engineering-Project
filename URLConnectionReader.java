87b2a796a76c1cbf56ec192568b43c0c2267f196
LandonVance <lvance@slu.edu>
2013-02-05 19:50:55 -0600

    Added necessary comments

--- URLConnectionReader.java
+++ URLConnectionReader.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 
 //from a tutorial at http://docs.oracle.com/javase/tutorial/networking/urls/readingWriting.html
 
-
+//This class connects to one or more URLs passed as arguments
+//It then outputs the entrie HTML source of these URLs to stdout
+//Output for a URL always starts with an HTML comment:
+//<!--Here begins url (url) -->
 public class URLConnectionReader {
   public static void main(String[] args) throws Exception {
     ArrayList urls = new ArrayList(Arrays.asList(args));
